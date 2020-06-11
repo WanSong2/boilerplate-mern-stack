c85464cb53a3b70c973c72d7811e2a5e99e4beb0
WanSong2 <thddhks11111@gmail.com>
2020-06-11 23:13:31 +0900

    2020.06.11

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -8,13 +8,13 @@ const { Meta } = Card;
 
 function LandingPage() {
 
-    const [Videos, setVideos] = useState({})
+    const [Videos, setVideos] = useState([])
 
     useEffect(() => {
         axios.get('/api/video/getVideos')
         .then(response => {
           if (response.data.success) {
-              //console.log(response);
+              console.log(response.data.videos);
               setVideos(response.data.videos);
           } else {
             alert('비디오 가져오기를 실패 했습니다.');
@@ -23,9 +23,36 @@ function LandingPage() {
     }, [])
 
 
-    const renderCards = Videos.map((v) => {
-        console.log(v);
-        return v;
+    const renderCards = Videos.map((video, index) => {
+
+        var minutes = Math.floor(video.duration / 60);
+        var seconds = Math.floor((video.duration - minutes * 60));
+
+        return <Col lg={6} md={8} xs={24}>
+            <div style={{ position: 'relative' }}>
+                <a href={`/video/${video._id}`} >
+                <img style={{ width: '100%' }} alt="thumbnail" src={`http://localhost:5000/${video.thumbnail}`} />
+                <div className=" duration"
+                    style={{ bottom: 0, right:0, position: 'absolute', margin: '4px',
+                    color: '#fff', backgroundColor: 'rgba(17, 17, 17, 0.8)', opacity: 0.8,
+                    padding: '2px 4px', borderRadius:'2px', letterSpacing:'0.5px', fontSize:'12px',
+                    fontWeight:'500', lineHeight:'12px' }}>
+                    <span>{minutes} : {seconds}</span>
+                </div>
+                </a>
+            </div><br />
+            <Meta
+                avatar={
+                    <Avatar src={video.writer.image} />
+                }
+                title={video.title}
+            />
+            <span>{video.writer.name} </span><br />
+            <span style={{ marginLeft: '3rem' }}> {video.views}</span>
+            - <span> {moment(video.createdAt).format("MMM Do YY")} </span>
+        </Col>
+
+
     })
 
 
@@ -34,8 +61,8 @@ function LandingPage() {
             <Title level={2} > Recommended </Title>
             <hr />
 
-            <Row gutter={16}>
-
+            <Row gutter={[16 , 32]}>
+                {renderCards}
             </Row>
         </div>
     )
