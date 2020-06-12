55f74edddcc0d087556a23651ad956f73f8c8a0c
WanSong2 <thddhks11111@gmail.com>
2020-06-12 14:23:54 +0900

    2020.06.12

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -8,8 +8,9 @@ const { Meta } = Card;
 
 function LandingPage() {
 
-    const [Videos, setVideos] = useState({})
+    const [Videos, setVideos] = useState([])
 
+    // Dom이 로드되자 마자 무엇이 할지를 설정
     useEffect(() => {
         axios.get('/api/video/getVideos')
         .then(response => {
@@ -23,9 +24,35 @@ function LandingPage() {
     }, [])
 
 
-    const renderCards = Videos.map((v) => {
-        console.log(v);
-        return v;
+    const renderCards = Videos.map((video, index) => {
+
+        var minutes = Math.floor(video.duration / 60);
+        var seconds = Math.floor(video.duration - minutes * 60);
+
+        return <Col lg={6} md={8} xs={24}>
+            <div style={{ position: 'relative' }}>
+                <a href={`/video/${video._id}`} >
+                <img style={{ width: '100%' }} alt="thumbnail" src={`http://localhost:5000/${video.thumbnail}`} />
+                <div className="duration"
+                    style={{ bottom: 0, right:0, position: 'absolute', margin: '4px', 
+                    color: '#fff', backgroundColor: 'rgba(17, 17, 17, 0.8)', opacity: 0.8, 
+                    padding: '2px 4px', borderRadius:'2px', letterSpacing:'0.5px', fontSize:'12px',
+                    fontWeight:'500', lineHeight:'12px' }}>
+                    <span>{minutes} : {seconds}</span>
+                </div>
+                </a>
+            </div><br />
+            <Meta
+                avatar={
+                    <Avatar src={video.writer.image} />
+                }
+                title={video.title}
+            />
+            <span>{video.writer.name} </span><br />
+            <span style={{ marginLeft: '3rem' }}> {video.views}</span>
+            - <span> {moment(video.createdAt).format("MMM Do YY")} </span>
+        </Col>
+
     })
 
 
@@ -35,7 +62,7 @@ function LandingPage() {
             <hr />
 
             <Row gutter={16}>
-
+                {renderCards}
             </Row>
         </div>
     )
